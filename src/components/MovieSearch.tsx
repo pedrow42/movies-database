6de788467eb352e8a5097fa01b4d451fede1fac0
Pedro Lopes <pedro_lopes98@hotmail.com>
2022-01-19 16:54:33 -0300

    changing directories

--- src/components/MovieSearch.tsx
+++ src/components/MovieSearch.tsx
@@ -1,12 +1,16 @@
 import { useState, useEffect } from "react"
 import { api } from "../api";
 import { MoviesType } from "../types/MoviesType";
-import { MovieContent } from "../components/MovieContent";
+import { useParams } from "react-router-dom";
+import { MovieContent } from "./MovieContent";
 import { AnimatedPage } from "../AnimatedPage";
 
-export const MoviesList = () =>{
+export const MovieSearch = () =>{
 
     const [movies, useMovies] = useState<MoviesType[]>([])
+
+    const idParams = useParams().id!
+    const idGenre = useParams().genre!
 
     useEffect(()=>{
         loadMovies();
@@ -19,15 +23,15 @@ export const MoviesList = () =>{
 
     return(
         <AnimatedPage>
-
             <div className="movieListContainer">
+                <h3 className="mb-5">{idGenre.toUpperCase()}</h3>
                 <div className='content'>
                     {movies.map((item, index)=>(
-                        <MovieContent title={item.title} poster={item.poster_path} id={item.id} key={index}/>
+                        item.genre_ids.indexOf(parseInt(idParams)) != -1 &&
+                            <MovieContent title={item.title} poster={item.poster_path} id={item.id} key={index}/>
                     ))}
                 </div>
             </div>
-
         </AnimatedPage>
         
     )
--- src/routes/RoutesList.tsx
+++ src/routes/RoutesList.tsx
@@ -1,11 +1,12 @@
 import { useRoutes } from "react-router-dom"
 import { MovieItem } from "../pages/MovieItem"
 import { MoviesList } from "../pages/MoviesList"
+import {MovieSearch} from "../components/MovieSearch"
 
 export const RoutesList = ()=>{
     return useRoutes([
         {path: "/", element: <MoviesList/>},
-        {path: "/movies/:genre/:id", element: <MoviesList/>},
+        {path: "/movies/:genre/:id", element: <MovieSearch/>},
         {path: "/movies/:id", element: <MovieItem />}
     ])
 }
